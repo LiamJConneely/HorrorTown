d2f3dfd37f6675bdac7b0f2d5a12da6cb40f8d76
LiamJConneely <liamjconneely20@gmail.com>
2020-09-27 20:31:44 +0100

    Camera switch and movement fixed

---
+++ Horror Town/Assets/cameraSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSwitch : MonoBehaviour
+{
+
+    public Camera cam1, cam2;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Cam1On()
+    {
+        cam1.enabled = true;
+        cam1.tag = "Main Camera";
+
+    }
+
+   public void Cam1Off()
+    {
+        cam1.enabled = false;
+
+
+    }
+
+    public void Cam2On()
+    {
+        cam2.enabled = true;
+        cam2.tag = "Main Camera";
+
+
+    }
+
+   public void Cam2Off()
+    {
+
+        cam2.enabled = false;
+    }
+}
---
+++ Horror Town/Assets/changeCamera.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public cameraSwitch cams;
+    public String methodname;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (methodname == "Camera 1")
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                cams.Cam1On();
+                cams.Cam2Off();
+
+            }
+          
+        }
+        
+        
+        if (methodname == "Camera 2")
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                cams.Cam2On();
+                cams.Cam1Off();
+            }
+         
+        }
+    }
+}
